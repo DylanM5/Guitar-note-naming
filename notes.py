26fbea5d7644ff790fd805e153765204ad3b423b
DylanM5 <69550936+DylanM5@users.noreply.github.com>
2023-03-30 16:35:01 -0400

    Add files via upload

---
+++ notes.py
@@ -0,0 +1,17 @@
+from random import seed
+from random import randint
+
+while True:
+
+    for _ in range(1):
+        fret = randint(0, 12)
+        print("Fret: " + str(fret))
+
+    for _ in range(1):
+        string = randint(1, 6)
+        print("String: " + str(string))
+
+        repeat = input("Do you want to repeat? (y/n)")
+
+        if repeat.lower() != "y":
+            break
