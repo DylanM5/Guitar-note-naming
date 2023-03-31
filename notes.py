fed0763ef84c963d47026dcfb07085f2ce34451e
DylanM5 <69550936+DylanM5@users.noreply.github.com>
2023-03-31 10:01:11 -0400

    gui update

--- notes.py
+++ notes.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 from random import randint
 
+BG_COLOR = "#222222"
+FG_COLOR = "#FFFFFF"
+
 
 def generate_numbers():
     fret = randint(0, 12)
     string = randint(1, 6)
-    fret_label.config(text="Fret: " + str(fret))
-    string_label.config(text="String: " + str(string))
+    fret_label.config(text="Fret: " + str(fret), highlightbackground=BG_COLOR)
+    string_label.config(text="String: " + str(string),
+                        highlightbackground=BG_COLOR)
 
 
 def repeat():
@@ -16,15 +20,24 @@ def repeat():
 root = tk.Tk()
 root.title("Random Note Generator")
 
-fret_label = tk.Label(root, text="Fret: ")
+root.configure(bg=BG_COLOR)
+
+font_style = ("TkDefaultFont", 30)
+label_kwargs = {"font": font_style, "fg": FG_COLOR, "bg": BG_COLOR}
+
+fret_label = tk.Label(root, text="Fret: ", **label_kwargs)
 fret_label.pack()
 
-string_label = tk.Label(root, text="String: ")
+string_label = tk.Label(root, text="String: ", **label_kwargs)
 string_label.pack()
 
-generate_button = tk.Button(root, text="Generate", command=generate_numbers)
+generate_button = tk.Button(
+    root, text="Generate", command=generate_numbers, bg=BG_COLOR, fg=FG_COLOR, font=font_style)
 generate_button.pack()
 
+for child in root.winfo_children():
+    if isinstance(child, tk.Frame):
+        child.configure(bg=BG_COLOR)
 
 root.after(1000, repeat)
 root.mainloop()
